6f0c82c224712a2989ec4513b295430a59109450
Slabpixel <developer@slabpixel.com>
2026-03-03 20:30:32 +0700

    Initial Commit

---
+++ KawanRusunku/KawanRusunkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KawanRusunkuApp.swift
+//  KawanRusunku
+//
+//  Created by SlabPixel Designer on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KawanRusunkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
